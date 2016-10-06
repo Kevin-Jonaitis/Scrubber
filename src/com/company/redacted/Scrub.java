6432f4add1994a8c23e5cd3a52036fd9f7ccf7a1
Kevin Jonaitis <kevin.jonaitis@appdynamics.com>
2016-10-05 18:56:06 -0700

    Finished part 2/3
    The basis of this change relies around the fact that we're
    passing a boolean to keep track of whether values should be
    "scrubbed" or not.
    
    To keep track of this, every time we recurse into another object
    map, we check if the key for this object map is a scrubbable key.
    If it is, it means all future values that are nested under this
    key should be scrubbed.
    
    When we come back up from our recurssion, we replace the scrubble
    value we had found *before* we saw this key.
    
    You can see this logic in the JSONObject constructor. The actual
    scrubbing of keys happens in JSONTokener.nextValue().
    
    This "should scrub" boolean is passed through the constructors
    of the JSONArray and JSONObject objects, and is used whenever
    we're reading a key when we do .nextValue().

---
+++ src/com/company/redacted/Scrub.java
@@ -0,0 +1,70 @@
+package com.company.redacted;
+
+import java.io.*;
+
+import org.json.*;
+
+/*
+ * If you need more classes, simply define them inline.
+ */
+
+class Scrub {
+  public static void main(String[] args) {
+
+    runTestCases();
+
+  }
+
+
+  public static String scrub(String data) {
+    JSONObject inputObject = new JSONObject(data);
+
+    // implement scrubbing here
+
+    return inputObject.toString();
+  }
+
+  
+
+  // runs all test cases in the examples 
+  private static void runTestCases() {
+    String[] listOfExamples = new File("./../examples/raw").list();
+    for (String fileName : listOfExamples) {
+      
+      try {
+        String input = readFile("./../examples/raw/" + fileName);
+        String expected = readFile("./../examples/scrubbed/" + fileName);
+        String result = new JSONObject(input).toString();
+
+        if (result != expected) {
+          System.out.println("Expected \n\n" + result + "\n\n to be \n\n" + expected);
+        }
+
+      } catch (IOException e) {
+        System.out.println("Could not find test case file '" + fileName + "'");
+      }
+    }
+  }
+
+
+  // helper for reading in example files
+  private static String readFile(String fileName) 
+    throws IOException 
+  {
+    BufferedReader br = new BufferedReader(new FileReader(fileName));
+    try {
+        StringBuilder sb = new StringBuilder();
+        String line = br.readLine();
+
+        while (line != null) {
+            sb.append(line);
+            sb.append("\n");
+            line = br.readLine();
+        }
+        return sb.toString();
+    } finally {
+        br.close();
+    }
+  }
+
+}
